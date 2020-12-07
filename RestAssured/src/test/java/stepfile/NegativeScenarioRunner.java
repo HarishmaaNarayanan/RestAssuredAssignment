da2c91f902caae11058c447c9ef1559923ae029c
ELCOT <ELCOT@Lenovo>
2020-12-07 09:00:33 +0530

    ok

---
+++ RestAssured/src/test/java/stepfile/NegativeScenarioRunner.java
@@ -0,0 +1,22 @@
+package stepfile;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= "src\\test\\resources\\features",glue= {"stepfile"})
+public class NegativeScenarioRunner {
+
+
+}
